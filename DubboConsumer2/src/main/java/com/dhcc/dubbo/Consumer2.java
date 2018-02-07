176cf0602a987b44a03fc950e11f694bf3ffa17e
Java2Mrs <z756684039@163.com>
2018-02-07 08:55:34 +0800

    测试

--- DubboConsumer2/src/main/java/com/dhcc/dubbo/Consumer2.java
+++ DubboConsumer2/src/main/java/com/dhcc/dubbo/Consumer2.java
@@ -9,17 +9,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  *  @author:ShenTai
  *  @date: 2018/2/6  14:51
  */
-public class Consumer2 implements Runnable{
+public class Consumer2{
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("consumer2.xml");
         context.start();
         System.out.println("consumer2 start");
         DemoService demoService = context.getBean(DemoService.class);
-        System.out.println(new Thread(new Consumer2("线程1")).start());
+        System.out.println(demoService.getPermissions(2L));
     }
 
-    public void run() {
-
-    }
 }
